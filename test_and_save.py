1e251c2c2b254323ef98f3707c51f4f80abe9f2c
mikayosef <mikayosef2@gmail.com>
2021-11-07 11:14:53 +0200

    third comit- added test examples

--- test_and_save.py
+++ test_and_save.py
@@ -6,10 +6,10 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
 
-def test_paint(pic_path,file_name,device,NET):
+def test_paint(pic_name, device, NET):
     with torch.no_grad():
+        pic_path = r"C:\Users\mikay\Documents\Study\paint_BW_images/" + pic_name + ".jpg"
         with Image.open(pic_path) as im:
-            
             pic_transform = transforms.ToTensor()
             pic = pic_transform(im)
             im = np.array(im)
@@ -17,35 +17,34 @@ def test_paint(pic_path,file_name,device,NET):
             plt.axis('off')
             plt.show()
 
-
             gray_test = pic
-            if pic.shape[0]>1:
-                gray_transform = torchvision.transforms.Grayscale(num_output_channels=1)
+            if pic.shape[0] > 1:
+                gray_transform = torchvision.transforms.Grayscale(
+                    num_output_channels=1)
                 gray_test = gray_transform(pic)
-            gray_test = torch.unsqueeze(gray_test,0).to(device)
+            gray_test = torch.unsqueeze(gray_test, 0).to(device)
 
             outputs = NET(gray_test)
 
-            im = save(outputs[0], file_name)
+            im = save(outputs[0], pic_name)
             plt.imshow(im)
             plt.axis('off')
-
 
             del outputs
             del gray_test
             torch.cuda.empty_cache()
 
 
+# pic_name = "mountain"
+# pic_path = r"C:\Users\mikay\Documents\Study\paint_BW_images/" +pic_name + ".jpg"
+
+# test_paint(pic_path, f"{pic_name}_pred",device, NET)
 
 
-
-
-
-
-def save(img, file_name):
+def save(img, pic_name):
     npimg = img.cpu().numpy()
     npimg = np.clip(npimg, 0, 1)
     npimg = np.transpose(npimg, (1, 2, 0))
     im = Image.fromarray(((npimg * 255).astype(np.uint8)))
-    im.save("{}.jpg".format(file_name))
+    im.save("{}_pred.jpg".format(pic_name))
     return im
