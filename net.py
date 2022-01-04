6d87c7ca5986fe93fe1f1a03a94e5126137b6d0e
mikayosef <89918702+mikayosef@users.noreply.github.com>
2022-01-04 09:47:48 +0200

    Update net.py

--- net.py
+++ net.py
@@ -29,14 +29,7 @@ class CONVNet(nn.Module):
         self.conv11 = nn.Conv2d(5, 3, kernel_size=3,
                                 padding=1, padding_mode='reflect')
 
-        # self.conv = []
-        # input_dim = 3
-        # output_dim = 15
-        # for i in range(10):
-        #     self.conv.append(nn.Sequential(nn.Conv2d(input_dim, output_dim, kernel_size=3, padding=1), nn.ReLU()))
-        #     input_dim = output_dim
-        #     output_dim = int(output_dim*1.1)
-        # self.conv = nn.Sequential(*self.conv)
+
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -54,7 +47,7 @@ class CONVNet(nn.Module):
         return x
 
 
-# small network with convolution and FC:
+
 
 class FULLNet(nn.Module):
 
